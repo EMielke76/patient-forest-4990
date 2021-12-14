8d79bf429f5f8bbbc0d5ef409a5909341ab39ef7
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-14 14:02:56 -0700

    Fix synatx error in test

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Movie Show' do
   let!(:universal) {Studio.create!(name: 'Universal Studios', location: 'Hollywood')}
-  let!(:raiders) {Movie.create!(name: 'The Last Crusade', creation_year: '1989', genre: 'Action Adventure', studio_id: universal.id)}
+  let!(:raiders) {Movie.create!(title: 'The Last Crusade', creation_year: '1989', genre: 'Action Adventure', studio_id: universal.id)}
   let!(:ford) {Actor.create!(name: 'Harrison Ford', age: '38' )}
   let!(:connery) {Actor.create!(name: 'Sean Connery', age: '55' )}
   let!(:davies) {Actor.create!(name: 'John Rhys-Davies', age: '47' )}
@@ -22,8 +22,8 @@ RSpec.describe 'Movie Show' do
 
     visit "/movies/#{raiders.id}"
 
-    expect(page).to have_content(raiders.name)
+    expect(page).to have_content(raiders.title)
     expect(page).to have_content(raiders.creation_year)
     expect(page).to have_content(raiders.genre)
-  end 
+  end
 end
