53d87ac6d842e638f6ee664483e0187ff153e9a9
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-14 13:32:03 -0700

    Create test for studio index display

---
+++ spec/features/studios/index_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Studio Index' do
+  let!(:universal) {Studio.create!(name: 'Universal Studios', location: 'Hollywood')}
+
+  before :each do
+    universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+    universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy')
+  end
+
+  it 'displays a studios name and location' do
+
+    visit '/studios'
+
+    expect(page).to have_content(universal.name)
+    expect(page).to have_content(universal.location)
+  end
+end
