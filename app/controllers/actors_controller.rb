b4bb3a9b9fd6858f942b08fcc4679d197ba16c1d
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-14 15:57:40 -0700

    Creat building blocks to solve problem

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,5 +2,6 @@ class ActorsController < ApplicationController
 
   def show
     @actor = Actor.find(params[:id])
+    require "pry"; binding.pry
   end
 end
--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,8 @@
 class Actor < ApplicationRecord
   has_many :movie_actors
   has_many :movies, through: :movie_actors
+
+  def self.co_actors
+    require "pry"; binding.pry
+  end
 end
