52e608a3565678a55eaae3da0b67358a6ab595e8
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-14 13:45:37 -0700

    Add test for studios#index displaying movie titles

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -6,13 +6,19 @@ RSpec.describe 'Studio Index' do
   before :each do
     universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
     universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy')
+
+      visit '/studios'
   end
 
   it 'displays a studios name and location' do
 
-    visit '/studios'
-
     expect(page).to have_content(universal.name)
     expect(page).to have_content(universal.location)
+  end
+
+  it 'also displays each studios movie titles' do
+
+    expect(page).to have_content("Raiders of the Lost Ark")
+    expect(page).to have_content("Shrek")
   end
 end
