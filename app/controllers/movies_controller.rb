98ed7832ee3a9a33874b2e6c87895394bd0b8f38
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-14 15:29:39 -0700

    Feat: Form to add Actor to a film  working as intended

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,5 +2,12 @@ class MoviesController < ApplicationController
 
   def show
     @movie = Movie.find(params[:id])
+    if params[:name].present?
+      actor = Actor.select {|actor| actor.name == params[:name]}
+      @movie.actors << actor
+      redirect_to "/movies/#{@movie.id}"
+    else
+      @movie
+    end
   end
 end
