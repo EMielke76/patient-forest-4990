bf1ba1d5b7d4eb8001a439809ae8498445afd4d1
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-14 14:20:53 -0700

    Feat: Movies #chronological_actors passing tests

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,9 @@
 class Movie < ApplicationRecord
   belongs_to :studio
   has_many :movie_actors
-  has_many :actors, through: :movie_actors 
+  has_many :actors, through: :movie_actors
+
+  def chronological_actors
+    actors.order(age: :asc)
+  end
 end
